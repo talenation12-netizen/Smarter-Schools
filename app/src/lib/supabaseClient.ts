5c50d50582e0137e07e1bf937a271d13d4bb95ef
Swordofdavid <tololotoo@gmail.com>
2026-05-04 09:26:39 +0300

    add students page from supabase

--- app/src/lib/supabaseClient.ts
+++ app/src/lib/supabaseClient.ts
@@ -6,15 +6,16 @@
 // );
 
 
-
-
-
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
+  console.error("Missing Supabase env vars:", {
+    url: supabaseUrl,
+    key: supabaseKey,
+  });
   throw new Error("Missing Supabase environment variables");
 }
 
