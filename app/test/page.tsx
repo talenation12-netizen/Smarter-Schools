dd16f66ff8704e6178a026b01c72a0aa1a1ca8b2
Swordofdavid <tololotoo@gmail.com>
2026-05-04 08:27:43 +0300

    add students page from supabase

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../src/lib/supabaseClient";
 
 export default function TestPage() {
   const [data, setData] = useState<any>(null);
